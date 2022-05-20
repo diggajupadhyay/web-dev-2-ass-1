302c695b48c54b4cde078fc78bbb10cb1d66fd63
Diggaj Upadhyay <dcozupadhyay@duck.com>
2022-05-20 15:56:00 +0800

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,86 +4,61 @@ function onblurName() {
     name.value = name.value.toUpperCase();
 }
 
-// Onsubmit event to submit the form and reload the window
-function onsubmitForm() {
+// Property Page form Validation
+function onsubmitForm() { // onsubmit event to submit the form and reload the window
+    var name = document.getElementById("name").value; // get the value of the name input field
+    var phone = document.getElementById("phone").value; // get the value of the phone input field
+    var address = document.getElementById("address").value; // get the value of the address input field
+    var price = document.getElementById("price").value; // get the value of the price input field
+
+    if (name == '') { // if the name is empty
+        alert("Please enter your name."); // alert the user
+        return false; // prevent form from submitting
+    }
+
+    if (phone == '') { // if the phone number is empty
+        alert("Please enter your phone number.");
+        return false; // prevent form from submitting
+    } else if (phone.length != 10) { // phone number must be more than 10 digits
+        alert("Please enter a valid phone number.");
+        return false;
+    }
+
+    if (address == '') { // if the address is empty
+        alert("Please enter your address.");
+        return false;
+    }
+
+    if (price == '') { // if the price is empty
+        alert("Please enter the price.");
+        return false;
+    } else if (price <= 0) { // price must be more than 0
+        alert("Please enter a valid price.");
+        return false;
+    }
     alert("Thank you. We are away right now. \n\nWe will contact you as soon as we see your query.");
-    setTimeout(function () {
-        window.location.reload();
-    },10);
 }
 
-// Property Page form Validation
-var form = document.getElementsByTagName('form')[0]; // Get the form element
-form.addEventListener('submit', function (onsubmitForm) { // e is the event
-    var name = document.getElementById('name').value; // get the value of the name input field
-    var phone = document.getElementById('phone').value; // get the value of the phone input field
-    var address = document.getElementById('address').value; // get the value of the address input field
-    var price = document.getElementById('price').value; // get the value of the price input field
+// Contact Form Validation
+function contactForm() {
+    var name = document.getElementById("name").value; // get the value of the name input field
+    var email = document.getElementById("email").value; // get the value of the email input field
+    var message = document.getElementById("message").value; // get the value of the message input field
+    var captcha = document.getElementById("captcha").value; // get the value of the captcha input field
 
-    // validate name
     if (name == '') { // if the name is empty
-        alert('Please enter your name.');
-        e.preventDefault(); // prevent form from submitting
+        alert("Please enter your name."); // alert the user
+        return false; // prevent form from submitting
     }
-
-    // validate phone number 
-    if (phone == '') { // if the phone number is empty
-        alert('Please enter your phone number.');
-        e.preventDefault();
-    } else if (phone.length != 10) { // phone number must be more than 10 digits 
-        alert('Please enter a valid phone number.');
-        e.preventDefault(); // prevent form from submitting
-    }
-
-    // validate address
-    if (address == '') { // if the address is empty
-        alert('Please enter your address.');
-        e.preventDefault(); // prevent form from submitting
-    }
-
-    // validate price
-    if (price == '') { // if the price is empty
-        alert('Please enter the price.');
-        e.preventDefault(); // prevent form from submitting
-    } else if (price <= 0) { // price must be more than 0
-        alert('Please enter a valid price.');
-        e.preventDefault(); // prevent form from submitting
-    }
-});
-
-// Contact Page form validation
-var form = document.getElementsByTagName('form')[1]; // Get the form element
-form.addEventListener('submit', function (onsubmitForm) { // e is the event
-    var name = document.getElementById('name').value; // get the value of the name input field
-    var email = document.getElementById('email').value; // get the value of the email input field
-    var message = document.getElementById('message').value; // get the value of the message input field
-
-    // validate name
-    if (name == '') { // if the name is empty
-        alert('Please enter your name.');
-        e.preventDefault(); // prevent form from submitting
-    }
-
-    // validate email with regex
     if (email == '') { // if the email is empty
-        alert('Please enter your email.');
-        e.preventDefault(); // prevent form from submitting
-    } else if (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) { // email must be in the correct format
-        alert('Please enter a valid email.');
-        e.preventDefault(); // prevent form from submitting
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(email)) { // if the email is not valid
+            alert("Please enter a valid email address.");
+            return false;
+        }
+    } else if (message == '') { // if the message is empty
+        // cannot be less than 10 characters
+        alert("Please enter a message.");
+        return false;
     }
-
-    // validate message
-    if (message == '') { // if the message is empty
-        alert('Please enter your message.');
-        e.preventDefault(); // prevent form from submitting
-    }
-
-    // check if the user has entered the correct number
-    var captcha = document.getElementById('math').value; // get the value of the captcha input field
-    if (captcha != '4') { // captcha must be 4
-        alert('You need to enter the correct number.');
-        e.preventDefault(); // prevent form from submitting
-    }
-});
-
+}
