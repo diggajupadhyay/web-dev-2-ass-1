2cd7e0fd451ef06a4b0f3c4f00885da26f1ea289
Diggaj Upadhyay <dcozupadhyay@duck.com>
2022-05-20 14:23:14 +0800

    moved policy script to index.html from script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,3 @@
-// on page alert
-// var policy = confirm("Inspections and applications are done via appointment only due to our company's covid policy.\n\nAre you sure you want to proceed?");
-// if (policy) {
-//     alert("Thank you for confirming.");
-// } else {
-//     alert("Sorry, you cannot visit the website unless you adhere to company's policy.");
-// }
-
 // onblur event to transform input to uppercase
 function onblurName() {
     var name = document.getElementById("name");
